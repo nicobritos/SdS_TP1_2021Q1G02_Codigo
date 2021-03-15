814810234812c40bc07ef08c82c99940e04b865c
nicbritos <nicbritos@hotmail.com>
2021-03-15 00:14:43 -0300

    Fixed scanner locale

--- java/src/main/java/ar/edu/itba/ss/y2021_q1_g123/parsers/ParticleParser.java
+++ java/src/main/java/ar/edu/itba/ss/y2021_q1_g123/parsers/ParticleParser.java
@@ -7,12 +7,14 @@ import ar.edu.itba.ss.y2021_q1_g123.models.Velocity;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public abstract class ParticleParser {
     public static ParticleSystem parseStatic(String filePath) throws FileNotFoundException {
         File file = new File(filePath);
         Scanner scanner = new Scanner(file);
+        scanner.useLocale(Locale.ENGLISH);
 
         int total = scanner.nextInt();
         int length = scanner.nextInt();
@@ -32,6 +34,7 @@ public abstract class ParticleParser {
     public static void parseDynamic(String filePath, ParticleSystem particleSystem, boolean parseVelocity) throws FileNotFoundException {
         File file = new File(filePath);
         Scanner scanner = new Scanner(file);
+        scanner.useLocale(Locale.ENGLISH);
 
         particleSystem.setTimeZero(scanner.nextInt());
 
